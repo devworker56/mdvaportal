52d066ee59761a3880bc1b3313478c747b9c5d0f
devworker56 <alainkavena@gmail.com>
2025-10-06 10:11:52 -0400

    updated the websocket_server.php and charities.php files to allow for sync with mobile app

--- api/websocket_server.php
+++ api/websocket_server.php
@@ -24,6 +24,10 @@ class MDVAWebSocket implements MessageComponentInterface {
     public function onMessage(ConnectionInterface $from, $msg) {
         $data = json_decode($msg, true);
         
+        if (!isset($data['type'])) {
+            return;
+        }
+        
         switch($data['type']) {
             case 'subscribe':
                 $this->subscriptions[$from->resourceId] = [
@@ -33,21 +37,50 @@ class MDVAWebSocket implements MessageComponentInterface {
                 echo "Client {$from->resourceId} subscribed as {$data['user_type']} ID {$data['user_id']}\n";
                 break;
                 
+            case 'subscribe_charities':
+                // Mobile app subscribing to charity updates
+                $this->subscriptions[$from->resourceId] = [
+                    'user_type' => 'mobile_app',
+                    'channel' => 'charity_updates'
+                ];
+                echo "Mobile app {$from->resourceId} subscribed to charity updates\n";
+                break;
+                
             case 'new_donation':
-                // Broadcast to relevant subscribers
-                foreach ($this->clients as $client) {
-                    if(isset($this->subscriptions[$client->resourceId])) {
-                        $sub = $this->subscriptions[$client->resourceId];
-                        if($sub['user_type'] == 'charity' && $sub['user_id'] == $data['charity_id']) {
-                            $client->send(json_encode([
-                                'type' => 'new_donation',
-                                'charity_id' => $data['charity_id'],
-                                'amount' => $data['amount'],
-                                'donor_id' => $data['donor_id']
-                            ]));
+                // Broadcast donation to relevant charity subscribers
+                $charity_id = $data['charity_id'] ?? null;
+                if ($charity_id) {
+                    foreach ($this->clients as $client) {
+                        if(isset($this->subscriptions[$client->resourceId])) {
+                            $sub = $this->subscriptions[$client->resourceId];
+                            if($sub['user_type'] == 'charity' && $sub['user_id'] == $charity_id) {
+                                $client->send(json_encode([
+                                    'type' => 'new_donation',
+                                    'charity_id' => $charity_id,
+                                    'amount' => $data['amount'],
+                                    'donor_id' => $data['donor_id'],
+                                    'timestamp' => $data['timestamp'] ?? date('Y-m-d H:i:s')
+                                ]));
+                            }
                         }
                     }
                 }
+                break;
+                
+            case 'new_charity':
+            case 'charity_update':
+                // Broadcast to all mobile apps subscribed to charity updates
+                $message_data = [
+                    'type' => 'charity_list_updated',
+                    'message' => 'Charity list has been updated',
+                    'charity_id' => $data['charity_id'] ?? null,
+                    'charity_name' => $data['charity_name'] ?? null,
+                    'action' => $data['action'] ?? 'updated',
+                    'timestamp' => $data['timestamp'] ?? date('Y-m-d H:i:s')
+                ];
+                
+                $this->broadcastToMobileApps($message_data);
+                echo "Broadcasted charity update: {$data['action']} for charity {$data['charity_id']}\n";
                 break;
         }
     }
@@ -62,6 +95,20 @@ class MDVAWebSocket implements MessageComponentInterface {
         echo "An error has occurred: {$e->getMessage()}\n";
         $conn->close();
     }
+
+    /**
+     * Broadcast message to all mobile apps subscribed to charity updates
+     */
+    private function broadcastToMobileApps($message) {
+        foreach ($this->clients as $client) {
+            if(isset($this->subscriptions[$client->resourceId])) {
+                $sub = $this->subscriptions[$client->resourceId];
+                if($sub['user_type'] == 'mobile_app' && $sub['channel'] == 'charity_updates') {
+                    $client->send(json_encode($message));
+                }
+            }
+        }
+    }
 }
 
 $server = IoServer::factory(
@@ -73,6 +120,7 @@ $server = IoServer::factory(
     8080
 );
 
-echo "WebSocket server running on port 8080\n";
+echo "MDVA WebSocket server running on port 8080\n";
+echo "Waiting for connections...\n";
 $server->run();
 ?>
